41d78770cf4e689f7e95f9b0ba7cc1d255844e1d
edgerunnergit <edgerunnergit@riseup.net>
2023-06-25 17:39:04 +0530

    added an xdp ebpf program

--- syspection-ebpf/src/main.rs
+++ syspection-ebpf/src/main.rs
@@ -3,9 +3,25 @@
 
 use aya_bpf::{
     helpers::*,
-    macros::tracepoint, 
-    programs::TracePointContext
-    };
+    bindings::*,
+    macros::{tracepoint, xdp}, 
+    programs::{TracePointContext, XdpContext},
+};
+use aya_log_ebpf::info;
+
+use core::mem;
+use network_types::{
+    eth::{EthHdr, EtherType},
+    ip::Ipv4Hdr,
+};
+
+#[xdp(name = "ip_scanner")]
+pub fn xdp_firewall(ctx: XdpContext) -> u32 {
+    match try_xdp_firewall(ctx) {
+        Ok(ret) => ret,
+        Err(_) => xdp_action::XDP_ABORTED,
+    }
+}
 
 #[tracepoint(name = "syspection")]
 pub fn syspection(ctx: TracePointContext) -> i32 {
@@ -13,6 +29,20 @@ pub fn syspection(ctx: TracePointContext) -> i32 {
         Ok(ret) => ret,
         Err(ret) => ret as i32,
     }
+}
+
+#[inline(always)]
+unsafe fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = mem::size_of::<T>();
+
+    if start + offset + len > end {
+        return Err(());
+    }
+
+    let ptr = (start + offset) as *const T;
+    Ok(&*ptr)
 }
 
 unsafe fn try_syspection(ctx: &TracePointContext) -> Result<i32, i64> {
@@ -42,6 +72,29 @@ unsafe fn try_syspection(ctx: &TracePointContext) -> Result<i32, i64> {
 
 
     bpf_printk!(b"exec: %s: %s %s %s %s", exec.as_ptr(), exec_comm, arg_buf[1].as_ptr(), arg_buf[2].as_ptr(), arg_buf[3].as_ptr());
+    Ok(0)
+}
+
+fn try_xdp_firewall(ctx: XdpContext) -> Result<u32, ()> {
+
+    let ethhdr: *const EthHdr = unsafe { ptr_at(&ctx, 0)? };
+    match unsafe { (*ethhdr).ether_type } {
+        EtherType::Ipv4 => {}
+        _ => return Ok(xdp_action::XDP_PASS),
+    }
+
+    let ipv4hdr: *const Ipv4Hdr = unsafe { ptr_at(&ctx, EthHdr::LEN)? };
+
+    let source = unsafe { (*ipv4hdr).src_addr };
+    let destination = unsafe { (*ipv4hdr).dst_addr };
+
+    let source_ip = source.to_le_bytes();
+    let destination_ip = destination.to_le_bytes();
+
+    info!(
+        &ctx, "SRC: {}.{}.{}.{} DST: {}.{}.{}.{}", source_ip[0], source_ip[1], source_ip[2], source_ip[3], destination_ip[0], destination_ip[1], destination_ip[2], destination_ip[3]
+    );
+
     Ok(0)
 }
 
--- syspection/src/main.rs
+++ syspection/src/main.rs
@@ -1,17 +1,23 @@
-use aya::programs::TracePoint;
+use anyhow::Context;
+use aya::programs::{TracePoint, Xdp, XdpFlags};
 use aya::{include_bytes_aligned, Bpf};
 use aya_log::BpfLogger;
+use clap::Parser;
 use log::{info, warn};
 use tokio::signal;
 
+#[derive(Debug, Parser)]
+struct Opt {
+    #[clap(short, long, default_value = "wlan0")]
+    iface: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+    let opt = Opt::parse();
+
     env_logger::init();
 
-    // This will include your eBPF object file as raw bytes at compile-time and load it at
-    // runtime. This approach is recommended for most real-world use cases. If you would
-    // like to specify the eBPF program at runtime rather than at compile-time, you can
-    // reach for `Bpf::load_file` instead.
     #[cfg(debug_assertions)]
     let mut bpf = Bpf::load(include_bytes_aligned!(
         "../../target/bpfel-unknown-none/debug/syspection"
@@ -27,6 +33,11 @@ async fn main() -> Result<(), anyhow::Error> {
     let program: &mut TracePoint = bpf.program_mut("syspection").unwrap().try_into()?;
     program.load()?;
     program.attach("syscalls", "sys_enter_execve")?;
+
+    let program2: &mut Xdp = bpf.program_mut("ip_scanner").unwrap().try_into()?;
+    program2.load()?;
+    program2.attach(&opt.iface, XdpFlags::default())
+        .context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE")?;
 
     info!("Waiting for Ctrl-C...");
     signal::ctrl_c().await?;
